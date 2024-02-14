a455b0d475fbb30be1ffb10e041ce6e7f2df494c
Himanshu <shanichaubey384@gmail.com>
2024-02-14 18:25:14 +0530

    asd

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from '../utils/error.js';
 import User from './../models/user.model.js';
 import bcryptjs from  'bcryptjs';
+import Jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next)=>{
 
@@ -22,5 +23,40 @@ export const signup = async (req,res,next)=>{
     } catch (error) {
        next(error);
     }
+    
+}
+
+export const signin  = async (req,res,next)=> {
+    // get the user info from request body
+    const { email, password } = req.body;
+
+    if( !email  || !password ) return next(errorHandler(400,"Username and Password is required"));
+
+    try {
+        // find the user in database by username
+        let validUser = await User.findOne({ email });
+                
+        if(!validUser) {
+            return next(errorHandler(401,"Invalid Username"));
+        }
+    
+        // compare the provided password with the hashed password in database
+        const validPassword =  bcryptjs.compareSync(password, validUser.password);    
+        
+
+        if (!validPassword) {
+            return next(errorHandler(401,'Invalid Password'));        
+        }
+        // Removing password from data
+        const { password : pass , ...rest} = validUser._doc;
+        
+        const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET,{ expiresIn: '7d' })
+        res.status(200).cookie('access_token', token , { httpOnly: true }).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+
+
     
 }
