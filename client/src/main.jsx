a293c6a8e6670dd7ea0374ce84b0ef3637d71cb0
Himanshu <shanichaubey384@gmail.com>
2024-02-15 17:57:07 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import {persistor} from './redux/store.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
+);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import  userReducer from './user/userSlice.js'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
+import persistStore from 'redux-persist/es/persistStore'
+
+const rootReducer = combineReducers({
+  user:userReducer
+})
+
+const persistConfig = {
+  key:"root",
+  version:1,
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store = configureStore({
-  reducer: { 
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(
+    { serializableCheck: false } // Immutable data warning in Redux Toolkit
+  )
 })
+
+export const persistor = persistStore(store);
