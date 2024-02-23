32171607410d8c0437611d2480803a5d65688e05
Himanshu <shanichaubey384@gmail.com>
2024-02-23 12:31:01 +0530

    Server Configured

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from  './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 dotenv.config()
 
@@ -18,12 +19,18 @@ mongoose.connect(process.env.MONGO)
 
 
 
-
+const __dirname = path.resolve();
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth',authRoutes)
 app.use('/api/posts', postRoutes);
 app.use('/api/comment', commentRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
